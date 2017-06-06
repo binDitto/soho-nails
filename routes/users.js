85bfc5e3f7c868e1bce448b3eee420819b245d54
Bin Tran <sudocode.bin@gmail.com>
2017-06-06 13:57:36 -0700

    Created nodejs routing, used nodemon for server control, initialized user routes

--- routes/users.js
+++ routes/users.js
@@ -7,4 +7,20 @@ router.get('/register', (req, res, next) => {
     res.send('REGISTER');
 });
 
+// USER AUTHENTICATION
+router.get('/authenticate', (req, res, next) => {
+    res.send('AUTHENTICATION');
+});
+
+// USER PROFILE
+router.get('/profile', (req, res, next) => {
+    res.send('PROFILE');
+});
+
+// USER VALIDATION
+router.get('/validate', (req, res, next) => {
+    res.send('VALIDATE');
+});
+
+
 module.exports = router;
--- server.js
+++ server.js
@@ -14,19 +14,13 @@ const app = express();
 const users = require('./routes/users');
 
 // PORT
-const port = 4000;
+const port = 3000;
 
 // CORS MIDDLEWARE - allows making request to api from different domain name || CHECK DOCUMENTATION FOR MORE INFO
 app.use(cors());
 
 // BODY PARER MIDDLEWARE - PARSER FORM DATA TO JSON SO WE CAN USE IT
-app.use(bodyParser.json)
-
-// RUN SERVER, type npm start in terminal because "start" in package.json set to run this file, OR TYPE NODEMON TO DO THIS AUTOMATICALLY
-app.listen(port, () => {
-    console.log('Server started on port ' + port);
-});
-
+app.use(bodyParser.json());
 
 //  USER ROUTES - routes after '/' have to go before '/' 
 app.use('/users', users);
@@ -36,6 +30,14 @@ app.get('/', (req, res) => {
 
     res.send('Invalid Endpoint');
 });
+
+
+// RUN SERVER, type npm start in terminal because "start" in package.json set to run this file, OR TYPE NODEMON TO DO THIS AUTOMATICALLY
+app.listen(port, () => {
+    console.log('Server started on port ' + port);
+});
+
+
 
 
 
