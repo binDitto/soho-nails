dcc876a396d2a4257e12fb63bd53f29934d261d3
Bin Tran <sudocode.bin@gmail.com>
2017-06-06 14:45:30 -0700

    Created a user model, and worked on register route to collect fake form data using postman, and adding it to mongo database

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/user');
 
 // User Registration - no need to add /users/register because this file is already associated with /users path
 
-router.get('/register', (req, res, next) => {
-    res.send('REGISTER');
+router.post('/register', (req, res, next) => {
+    let newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    User.addUser(newUser, (err, user) => {
+        if(err) {
+            res.json({success: false, msg: 'Failed to register user'});
+        } else {
+            res.json({success: true, msg: 'User registered'});
+        }
+    });
 });
 
 // USER AUTHENTICATION
@@ -16,11 +33,5 @@ router.get('/authenticate', (req, res, next) => {
 router.get('/profile', (req, res, next) => {
     res.send('PROFILE');
 });
-
-// USER VALIDATION
-router.get('/validate', (req, res, next) => {
-    res.send('VALIDATE');
-});
-
 
 module.exports = router;
