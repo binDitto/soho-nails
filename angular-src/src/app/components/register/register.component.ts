b9c81fed957a41d0fedd985db5b16b1c34322feb
Bin Tran <sudocode.bin@gmail.com>
2017-06-19 01:03:28 -0700

    registration component now actually works and communicates with backend and authorizes user registration

--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {ValidateService} from '../../services/validate.service';
 import { FlashMessagesService } from 'angular2-flash-messages';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -15,7 +17,10 @@ export class RegisterComponent implements OnInit {
   password: String;
 
   constructor(private validateService: ValidateService,
-              private flashMessage: FlashMessagesService) { } //Always inject Service in here to use
+              private flashMessage: FlashMessagesService,
+              private authService: AuthService,
+              private router: Router
+              ) { } //Always inject Service in here to use
 
   ngOnInit() {
   }
@@ -40,5 +45,17 @@ export class RegisterComponent implements OnInit {
       this.flashMessage.show('Please use valid email', {cssClass: 'alert-danger', timeout: 3000});
       return false;
     }
+
+    //Register User
+    this.authService.registerUser(user).subscribe(data => {
+      if(data.success){
+        this.flashMessage.show('You are now registered and can log in', {cssClass: 'alert-success', timeout: 3000});
+        this.router.navigate(['/login']);
+      } else {
+        this.flashMessage.show('Something wong', {cssClass: 'alert-danger', timeout: 3000});
+        this.router.navigate(['/register']);
+      }
+    });
+
   }
 }
