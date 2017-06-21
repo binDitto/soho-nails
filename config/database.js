11a492be0612680e9f00351ea3558528c6a0f9e9
Bin Tran <sudocode.bin@gmail.com>
2017-06-20 23:57:52 -0700

    connect to db last time

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: 'mongodb://localhost:27017/sohonails',
+    database: 'mongodb://heroku_fhck86zq:qo962n62ikvqfohh83uc16doim@ds139428.mlab.com:39428/heroku_fhck86zq',
     secret: 'sohosecret'
 }
