3593693f0429bcfcfec05feb152eafbd29c32a33
Bin Tran <sudocode.bin@gmail.com>
2017-06-20 23:30:55 -0700

    set up database to production.. with mlab

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: 'mongodb://localhost:27017/sohonails',
+    database: 'mongodb://bin:Oitroi@ds051838.mlab.com:51838/soho-nails' //'mongodb://localhost:27017/sohonails',
     secret: 'sohosecret'
 }
