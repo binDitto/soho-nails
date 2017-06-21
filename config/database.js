fa9ba735a70770277d4e77ad49cc87df44c0d17e
Bin Tran <sudocode.bin@gmail.com>
2017-06-20 23:39:41 -0700

    relinked db

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: 'mongodb://bin:Oitroi@ds133932.mlab.com:33932/heroku_fh1xsgdj' //'mongodb://localhost:27017/sohonails',
+    database: 'mongodb://bin:Oitroi@ds133932.mlab.com:33932/heroku_fh1xsgdj' 
     secret: 'sohosecret'
 }
