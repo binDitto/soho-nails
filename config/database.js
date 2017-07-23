52f632f3a5bff3eb8a36824a7f7b26c0ca553de2
Bin Tran <sudocode.bin@gmail.com>
2017-07-23 02:02:30 -0700

    Changed config/database database for mongolab to go first

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database:  'mongodb://localhost:27017/sohonails' || 'mongodb://heroku_fhck86zq:qo962n62ikvqfohh83uc16doim@ds139428.mlab.com:39428/heroku_fhck86zq' ,
+    database:  'mongodb://heroku_fhck86zq:qo962n62ikvqfohh83uc16doim@ds139428.mlab.com:39428/heroku_fhck86zq' || 'mongodb://localhost:27017/sohonails',
     secret: 'sohosecret'
 }
